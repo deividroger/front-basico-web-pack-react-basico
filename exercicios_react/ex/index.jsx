1079941a7cada00342e92f2bc1718239dbeab9ef
= <=>
2019-02-11 17:16:11 -0600

    Exercicios de react-redux

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {compineReducers, createStore, combineReducers} from 'redux'
+import { Provider } from 'react-redux'
 import Field from './field'
+import FieldReducer from './fieldReducer'
 
+const reducers = combineReducers({
+    field: FieldReducer
+})
 
 ReactDOM.render(
-    <Field initialValue='Teste' />
-    
+   <Provider store={createStore(reducers)}>
+
+        <Field initialValue='Teste' />
+    </Provider>  
 ,document.getElementById('app'))
 
 
